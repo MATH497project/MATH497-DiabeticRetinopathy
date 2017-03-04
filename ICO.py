df6680258d2635fd3d86d0b9abaa967b770eacea
Richard Osborn <richardosborn@mac.com>
2017-03-04 13:37:56 -0600

    Add all_person_data table

--- ICO.py
+++ ICO.py
@@ -10,14 +10,16 @@ class Data:
                        'SL_Lens_for_Enc', 'SNOMED_problem_list', 
                        'systemic_disease_for_Enc', 'systemic_disease_list']
         self.__data = {}
-        self.__normdata = {"all_encounter_data" : None}
+        self.__normdata = {"all_encounter_data" : None,
+                           "all_person_data" : None}
         for name in table_names:
             self.__data[name] = pd.read_pickle(filepath + name + '.pickle')
 
     def __getitem__(self,name):
-                self.create_enc_table()
         if self.__normdata.has_key(name):
             if self.__normdata.get(name) is None:
+                if name == "all_encounter_data": self.create_enc_table()
+                if name == "all_person_data": self.create_person_table()
             return self.__normdata[name]
         
         return self.__data.__getitem__(name)
@@ -28,36 +30,88 @@ class Data:
     def create_enc_table(self):
         # Helper function to convert string values to floats
         # 87 Glucose column have BP values which this function ignores and assigns NaN
-        def func(x):
+        # TODO: Swap the erroneous column values. Filter out outliers
+        NaN = float("NaN")
+        def to_float(x):
             try:
                 return float(x)
             except ValueError:
-                return float("NaN")
+                return NaN
                 
         # Split up the BP field into Systolic and Diastolic readings
         d_enc = self.__data["all_encounter_data"]
-        pattern = re.compile("(\d+)\s*\/\s*(\d+)")
-        temp = d_enc["BP"].str.extract(pattern, expand=True)
-        d_enc["BP_Systolic"] = temp[0]
-        d_enc["BP_Diastolic"] = temp[1]
-        temp = None
+        pattern = re.compile("(?P<BP_Systolic>\d+)\s*\/\s*(?P<BP_Diastolic>\d+)")
+        d_enc = pd.merge(d_enc, d_enc["BP"].str.extract(pattern, expand=True),
+                         left_index=True, right_index=True)
         
-        # Drop columns with multiple values for a single Enc_ID (and drop Enc_Nbr)
-        temp = d_enc.drop(["A1C",
-                           "BMI",
-                           "BP",
-                           "BP_Systolic",
-                           "BP_Diastolic",
-                           "Glucose",
-                           "Enc_Nbr"], axis=1).drop_duplicates().set_index("Enc_ID", verify_integrity=True)
-
+        # Drop columns with multiple values for a single Enc_ID (and drop Enc_ID, Person_ID)
         # When multiple values are observed, take the mean and merge them back into the complete table
-        self.__normdata["all_encounter_data"] =\
-            temp.merge(d_enc.loc[:,["Enc_ID",
-                                    "A1C",
-                                    "BMI",
-                                    "Glucose",
-                                    "BP_Systolic",
-                                    "BP_Diastolic"]].groupby("Enc_ID").agg(lambda x: np.mean(x.map(func))),
-                       how='outer', left_index=True, right_index=True)
+        self.__normdata["all_encounter_data"] = \
+            pd.merge(d_enc.drop(["A1C",
+                                 "BMI",
+                                 "BP",
+                                 "BP_Systolic",
+                                 "BP_Diastolic",
+                                 "Glucose",
+                                 "Enc_ID",
+                                 "Person_ID"], axis=1).drop_duplicates().set_index("Enc_Nbr"),
+                     d_enc.loc[:,["Enc_Nbr",
+                                  "A1C",
+                                  "BMI",
+                                  "Glucose",
+                                  "BP_Systolic",
+                                  "BP_Diastolic"]].groupby("Enc_Nbr").agg(lambda x: np.mean(x.map(to_float))),
+                     left_index=True, right_index=True)
         
+        # Add diagnoses to table
+        dEI = self.__data["ICD_for_Enc"].loc[:,["Enc_Nbr", "Diagnosis_Code_ID"]]
+        
+        # Manually fix erroneous values
+        dEI.loc[90899]="367.4"
+        dEI.loc[168442]="362.3"
+        
+        # Diabetes is under 250.* and 362.0.* for ICD9 and E08,E09,E10,E11,E13 for ICD10
+        dEI["DM"]=dEI["Diagnosis_Code_ID"].str.contains("^250.*|^362\.0.*|^E(?:0[89]|1[013])(?:\.[A-Z0-9]{1,4})?")
+
+        # Macular edema is under 362.07 for ICD9 and E(08|09|10|11|13).3([1-5]1|7) for ICD10
+        dEI["ME"]=dEI["Diagnosis_Code_ID"].str.contains("^362\.07|^E(?:0[89]|1[013])\.3(?:[1-5]1|7).*")
+
+        # Mild Nonproliferative Diabetic Retinopathy is under 362.04 for ICD9 and E(08|09|10|11|13).32
+        dEI["mNPDR"]=dEI["Diagnosis_Code_ID"].str.contains("^362\.04|^E(?:0[89]|1[013])\.32.*")
+
+        # Moderate Nonproliferative Diabetic Retinopathy is under 362.05 for ICD9 and E(08|09|10|11|13).33
+        dEI["MNPDR"]=dEI["Diagnosis_Code_ID"].str.contains("^362\.05|^E(?:0[89]|1[013])\.33.*")
+
+        # Severe Nonproliferative Diabetic Retinopathy is under 362.06 for ICD9 and E(08|09|10|11|13).34
+        dEI["SNPDR"]=dEI["Diagnosis_Code_ID"].str.contains("^362\.06|^E(?:0[89]|1[013])\.34.*")
+
+        # Proliferative Diabetic Retinopathy is under 362.02 for ICD9 and E(08|09|10|11|13).35
+        dEI["PDR"]=dEI["Diagnosis_Code_ID"].str.contains("^362\.02|^E(?:0[89]|1[013])\.35.*")
+
+        dEI["DR"]=dEI["mNPDR"] & dEI["MNPDR"] & dEI["SNPDR"] & dEI["PDR"]
+        
+        self.__normdata["all_encounter_data"] = \
+            pd.merge(self.__normdata["all_encounter_data"],
+                     dEI.drop(["Diagnosis_Code_ID"],axis=1).groupby("Enc_Nbr").any(),
+                     left_index=True, right_index=True)
+                     
+    def create_person_table(self):
+        d_enc = self["all_encounter_data"]
+        
+        # Average the past year of data
+        f = lambda x: x[x["Enc_Date"]>=x["Enc_Date"].max() - pd.DateOffset(years=1)].drop(["Person_Nbr","Enc_Date"],axis=1).mean()
+        self.__normdata["all_person_data"] = \
+            pd.merge(self.__data["demographics"].set_index("Person_Nbr"),
+                     d_enc.loc[:,["Enc_Date",
+                                  "Person_Nbr",
+                                  "A1C",
+                                  "BMI",
+                                  "Glucose",
+                                  "BP_Systolic",
+                                  "BP_Diastolic"]].groupby("Person_Nbr").apply(f),
+                     left_index=True, right_index=True)
+        # Combine all diagnoses
+        self.__normdata["all_person_data"] = \
+            pd.merge(self.__normdata["all_person_data"],
+                     d_enc.loc[:,["Person_Nbr","DM","DR","ME","MNPDR","PDR","SNPDR","mNPDR"]].groupby("Person_Nbr").any(),
+                     left_index=True, right_index=True)
