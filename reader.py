53f801aec4bb4a96ba2ea7ca60aa9a7fb5476fff
Dan <dliu34@hawk.iit.edu>
2017-03-05 12:05:26 -0600

    Add connection codes to google drive and reader object

---
+++ reader.py
@@ -0,0 +1,31 @@
+from connect_to_drive import check_and_update
+import numpy as np
+import pandas as pd
+import re
+
+class Data:
+    def __init__(self, filepath):
+        table_file_name_dict = {'all_encounter_data': 'all_encounter_data.pickle',
+                        'demographics': 'demographics.pickle',
+                        'encounters': 'encounters.pickle',
+                        'family_hist_for_Enc': 'family_hist_for_Enc.pickle',
+                        'family_hist_list': 'family_hist_list.pickle',
+                        'ICD_for_Enc': 'ICD_for_Enc.pickle',
+                        'macula_findings_for_Enc': 'macula_findings_for_Enc.pickle',
+                        'SL_Lens_for_Enc': 'SL_Lens_for_Enc.pickle',
+                        'SNOMED_problem_list': 'SNOMED_problem_list.pickle',
+                        'systemic_disease_for_Enc': 'systemic_disease_for_Enc.pickle',
+                        'systemic_disease_list': 'systemic_disease_list.pickle'}
+
+        check_and_update(table_file_name_dict, filepath)
+
+        self.__data = {}
+
+        for k,v in table_file_name_dict.items():
+            self.__data[k] = pd.read_pickle(filepath + v)
+
+    def __getitem__(self,name):
+        return self.__data.__getitem__(name)
+
+    def get_underlying(self):
+        return self.__data
